67b56b6f0182e0151c9906ea625b7a7107d6c87a
ashut <ashut@Ashutosh>
2022-11-09 02:25:47 +0530

    Fix: Naming

--- 001-Hibernate_One_To_One-2/src/main/java/info/ashutosh/reposetory/EmployeeReposetory.java
+++
@@ -1,9 +0,0 @@
-package info.ashutosh.reposetory;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import info.ashutosh.entity.User;
-
-public interface EmployeeReposetory extends JpaRepository<User, Long> {
-
-}
